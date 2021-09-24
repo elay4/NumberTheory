61e2e671e1b9829ee1bb3829dfbf16d2f1b538de
elay4 <54080807+elay4@users.noreply.github.com>
2021-09-24 14:32:16 +0300

    Euclidean Algorithm

--- Euclidean_Algorithm.py
+++ Euclidean_Algorithm.py
@@ -1,7 +1,8 @@
 # finding the gcd of two natural numbers by the euclidean algorithm
 # consensus: a,b two natural numbers where a < b
+import sys
 
-def divide_with_reminder(a, b):
+def divide_with_remainder(a, b):
     # b = a * q + r
     r = b % a
     q = int(b / a)
@@ -10,7 +11,7 @@ def divide_with_reminder(a, b):
 def euclidean_algo(a, b):
     if a == 0:
         return b
-    (q, r) = divide_with_reminder(a, b)
+    (q, r) = divide_with_remainder(a, b)
     return euclidean_algo(r, a)
 
 def extended_euclidean_algo(a, b):
@@ -20,12 +21,13 @@ def extended_euclidean_algo(a, b):
     (s_prev, s) = (0, 1)
     (t_prev, t) = (1, 0)
     while r != 0:
-        (q, res) = divide_with_reminder(r, r_prev)
+        (q, res) = divide_with_remainder(r, r_prev)
         (r_prev, r) = (r, res)
         (s_prev, s) = (s, s_prev - q * s)
         (t_prev, t) = (t, t_prev - q * t)
     return (r_prev, s_prev, t_prev)
 
+# returns (gcd, x, y) where x * a + y * b = gcd for a the min and b the max
 def extended_gcd(num1, num2):
     a = min(num1, num2)
     b = max(num1, num2)
@@ -36,8 +38,7 @@ def gcd(num1, num2):
     b = max(num1, num2)
     return euclidean_algo(a, b)
 
-num1, num2 = 873, 5418
 
-print(gcd(num1, num2))
-(d, x, y)=extended_gcd(num1, num2)
-print(x * num1 + y * num2, "and the gcd:", d)
+(num1, num2) = tuple(map(int, sys.argv[1:])) 
+print('The gcd of:', num1, ',', num2, 'is:', gcd(num1, num2))
+print('The extended gcd of:', num1,',', num2, 'is:', extended_gcd(num1, num2))
